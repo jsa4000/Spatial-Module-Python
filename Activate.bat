3001950976a6ecc02a9b1ce1e6840ae7bba80299
jsa4000 <jsa4000@gmail.com>
2017-05-04 10:34:18 +0200

    Initial update

--- Activate.bat
+++ Activate.bat
@@ -6,7 +6,7 @@
 call C:/Users/javier.santos/AppData/Local/Continuum/Python-5.5.3/envs/python_env_gisdem/Scripts/activate.bat
 
 :: run Visual Studio Code or whatever inside the current virtual environment
-:: start C:\jsantos\Software\VSCode\Code.exe
+start C:\jsantos\Software\VSCode\Code.exe
 
-::exit
+exit
 
