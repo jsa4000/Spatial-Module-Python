def74c45de9ddf86b9a18aace1c5624576fbb09e
Javier Santos <jsa4000@gmail.com>
2017-05-06 17:46:30 +0200

    Some update in home laptop

---
+++ Launch2.bat
@@ -0,0 +1,13 @@
+:: Open in Visual studio code using current virtual environment
+
+:: Activate desired python virtual environment
+:: call vs start vs nothing
+:: call will execute the sentence remaining inside the current cmd
+call activate tensorflow
+
+:: run Visual Studio Code or whatever inside the current virtual environment
+:: start code ./
+code ./
+
+exit
+
