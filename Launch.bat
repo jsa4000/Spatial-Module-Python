16c01917c4ab35c765bcdde40e576c03b078ab1f
jsa4000 <jsa4000@gmail.com>
2017-05-08 15:27:20 +0200

    Added some files related to Google Earth engine

--- Launch.bat
+++ Launch.bat
@@ -3,10 +3,11 @@
 :: Activate desired python virtual environment
 :: call vs start vs nothing
 :: call will execute the sentence remaining inside the current cmd
+ 
 call C:/Users/javier.santos/AppData/Local/Continuum/Python-5.5.3/envs/python_env_gisdem/Scripts/activate.bat
 
 :: run Visual Studio Code or whatever inside the current virtual environment
-start C:\jsantos\Software\VSCode\Code.exe
+start C:\jsantos\Software\VSCode\Code.exe ./
 
 exit
 
---
+++ gisdem/gmaps.py
@@ -0,0 +1,2 @@
+
+# 4/bdZVz3MkMiatYAAkccUpx1RrkK2ewnrbM5YEdF8-1zk
