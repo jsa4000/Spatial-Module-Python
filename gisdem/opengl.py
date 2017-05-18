8de12ddb3588900cf3aae70086bf1da0e748d40b
jsa4000 <jsa4000@gmail.com>
2017-05-18 17:11:18 +0200

    added camera

--- gisdem/opengl.py
+++ gisdem/opengl.py
@@ -1,4 +1,5 @@
 import os
+import math
 import ctypes
 import numpy as np
 import pandas as pd
@@ -6,10 +7,12 @@ import OpenGL.GL as GL
 import OpenGL.GL.shaders
 import pygame
 from PIL import Image
-from pyrr import Quaternion, Matrix44, Vector3
+from pyrr import Quaternion, matrix44, Matrix44, Vector3
 
 
 # https://github.com/adamlwgriffiths/Pyrr/tree/master/pyrr
+# https://www.opengl.org/discussion_boards/showthread.php/199031-How-to-sort-draw-and-shader-calls-for-multiple-models
+# https://www.khronos.org/opengl/wiki/Vertex_Specification_Best_Practices
 
 # Great useful resource to learn OpenGL and all the concepts needed for understanding
 # ligths, materials, shaders, transformations, etc..
@@ -399,6 +402,8 @@ class Geometry:
         self.shader = shader
         self.mode = mode
         self.usage = usage
+        # Create new properties
+        self.transform = Transform()
         # Attributes dictionary to store the columns for each component
         self._pointAttribCols = {}
         self._primAttribCols = {}
@@ -608,17 +613,17 @@ ShaderTypes = {
 
 # Transforms types availabile in shader
 TransformTypes = {
-    "WORLD_MATRIX"        : { name:"world_matrix",      size:16, dtype:np.float32 }, 
-    "VIEW_MATRIX"         : { name:"view_matrix",       size:16, dtype:np.float32 },    
-    "PROJECTION_MATRIX"   : { name:"projection_matrix", size:16, dtype:np.float32 }
+    "WORLD_MATRIX"        : { "name":"world_matrix",      "size":16, "dtype":np.float32 }, 
+    "VIEW_MATRIX"         : { "name":"view_matrix",       "size":16, "dtype":np.float32 },    
+    "PROJECTION_MATRIX"   : { "name":"projection_matrix", "size":16, "dtype":np.float32 }
     }
 
 # Transforms types availabile in shader
 GeometryAttributeTypes = {
-    "TEXTURE_COORDINATES"   : { name:"v_textcoord", size:2, dtype:np.float32 },       
-    "NORMAL"                : { name:"v_normal",    size:3, dtype:np.float32 },      
-    "POSITION"              : { name:"v_pos",       size:3, dtype:np.float32 },     
-    "COLOR"                 : { name:"v_color",     size:4, dtype:np.float32 }
+    "TEXTURE_COORDINATES"   : { "name":"v_textcoord", "size":2, "dtype":np.float32 },       
+    "NORMAL"                : { "name":"v_normal",    "size":3, "dtype":np.float32 },      
+    "POSITION"              : { "name":"v_pos",       "size":3, "dtype":np.float32 },     
+    "COLOR"                 : { "name":"v_color",     "size":4, "dtype":np.float32 }
     }
 
 class Shader:
@@ -707,7 +712,7 @@ class Shader:
     def _get_location_uniforms(self):
         for key,value in Shader.uniform_transforms.items():
             # Get the location for the curren shader loaded
-            self._uniforms[key] = GL.glGetUniformLocation(self._program, value.name)
+            self._uniforms[key] = GL.glGetUniformLocation(self._program, value["name"])
 
     def _bind_location_attributes(self):
         pass
@@ -1008,8 +1013,8 @@ class Transform:
         # Create translation matrix transformation
         translation = Matrix44.from_translation(self.position)
 
-        # Compute transformation matrix
-        return scale * rotation * translation
+        # Compute transformation matrix. convert to float32
+        return  np.array(scale * rotation * translation,dtype=np.float32)
 
     def transform(self, point):
         """
@@ -1020,6 +1025,132 @@ class Transform:
         matrix = self.model
         # transform our point by the matrix to model-view
         return matrix * self._get_Vector3(point)
+
+    def __enter__(self):
+        # Enter will always return the object itself. Use with With expressons
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Clean all the memery stored
+        self._dispose()
+
+    def _dispose(self):
+        pass
+
+def convert(value, dtype=np.float32):
+    return np.array(value,dtype)
+
+class Camera:
+    """
+        This class will manage the basic functionality that can be
+        performed with a camera.
+
+        This class uses pyrr module that it's a packadge with many
+        operations that can be used directly with OpenGL. In this class
+        the selected approach will be Object Oriented because its features.
+        Documentation can be founf in the following link:
+        
+        https://github.com/adamlwgriffiths/Pyrr
+
+        Parameters:
+            default perspective, rotation and scale can be set intially.
+        
+        To-Do:
+            Pivot implementation. So it's rotate based on a point.
+            Advanced transformations such as shear, bend, twist, et..
+
+    """
+    def __init__(self, position=[0.0,0.0,-3.0], fov=70.0, aspect=1.33, zNear=0.01, zFar=1000.0):
+        # Create private members for the setters (properties)
+        # View Matrix
+        self.__position = convert(position)
+        self._forward = convert([0.0,0.0,1.0])
+        self._up = convert([0.0,1.0,0.0])
+        # Prejection Matrix
+        self._fov = fov
+        self._aspect = aspect
+        self._zNear = zNear
+        self._zFar = zFar
+        
+        # Initiali<e variables and Window
+        self._initialize()
+
+    def _initialize(self):
+        pass
+
+    def pan(self, value):
+        """
+            Rotate using up direction
+        """
+        pass
+
+    def tilt(self, value):
+        """
+            Rotate using cross product between up and forward vectors.
+        """
+        pass
+
+    def roll(self, value):
+        """
+            Rotate using the forward direction
+        """
+        pass
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = convert(value)
+
+    def setPerspective(fov=70.0, aspect=1.33, zNear=0.01, zFar=1000.0):
+        """
+            Redefine the perspective view of the Camera
+        """
+        self._fov = fov
+        self._aspect = aspect
+        self._zNear = zNear
+        self._zFar = zFar
+       
+    @property
+    def view(self):
+        return self.lookAt(self.position,self.position + self._forward, self._up)
+
+    def lookAt(self, position, target, up):
+        ez = position - target
+        ez = ez / np.linalg.norm(ez)
+
+        ex = np.cross(up, ez)
+        ex = ex / np.linalg.norm(ex)
+
+        ey = np.cross(ez, ex)
+        ey = ey / np.linalg.norm(ey)
+
+        rmat = np.eye(4)
+        rmat[0][0] = ex[0]
+        rmat[0][1] = ex[1]
+        rmat[0][2] = ex[2]
+
+        rmat[1][0] = ey[0]
+        rmat[1][1] = ey[1]
+        rmat[1][2] = ey[2]
+
+        rmat[2][0] = ez[0]
+        rmat[2][1] = ez[1]
+        rmat[2][2] = ez[2]
+
+        tmat = np.eye(4)
+        tmat[0][3] = -_eye[0]
+        tmat[1][3] = -_eye[1]
+        tmat[2][3] = -_eye[2]
+
+        return np.dot(rmat, tmat).transpose()
+
+
+    @property
+    def projection(self):
+        return matrix44.create_perspective_projection_matrix(self._fov,self._aspect,self._zNear,self._zFar)
 
     def __enter__(self):
         # Enter will always return the object itself. Use with With expressons
@@ -1054,7 +1185,8 @@ if __name__ == "__main__":
        
         #georaw = cube3D()
         georaw = Triangle()
-        
+        # Create a Camera
+        camera = Camera([0.0,0.0,-3.0],70.0,800/600,0.01,1000)
         # Create a texture
         texture = Texture("./shaders/texture.png")
         # Create the default shader
@@ -1068,6 +1200,9 @@ if __name__ == "__main__":
         geo.addPointAttrib("UV",georaw[3], 2)
         #geo.addPoints(vertices, 4)
         geo.update()
+
+        # Create a counter
+        counter = 0
          # Start the Main loop for the program
         while not display.isClosed:     
             # Manage the event from the gui
@@ -1085,6 +1220,20 @@ if __name__ == "__main__":
             shader.use()
             # Use the current texture after the shader
             texture.bind(0)
+            # Perform some motion to the object
+            sincount = math.sin(counter)
+            coscount = math.cos(counter)
+
+            geo.transform.position.x = sincount
+            geo.transform.rotation.z = counter*50
+            geo.transform.scale = [coscount,coscount,coscount]
+
+            counter += 0.01;
+
+            shader.update("WORLD_MATRIX",geo.transform.model)
+            shader.update("VIEW_MATRIX",camera.view)
+            shader.update("PROJECTION_MATRIX",camera.projection)
+
             # Render the  geometry
             geo.render()
             # End Use the current Shader configuration
--- shaders/default_shader.vs
+++ shaders/default_shader.vs
@@ -5,13 +5,15 @@ in vec4 Cd;
 in vec2 UV;
 
 uniform mat4 world_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
 
 out vec4 oCd;
 out vec2 oUV;
 
 void main()
 {
-   gl_Position = world_matrix * position;
+   gl_Position = projection_matrix * view_matrix * world_matrix * position;
    oCd = Cd;
    oUV = UV;
 }
